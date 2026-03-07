8548a52adf16cbafd2aee2aaae00b81831691ec5
hermes <hermes.fpga.dev@gmail.com>
2026-03-07 11:55:43 +0100

    Renamed top file to match project name

---
+++ src/hdl/kria_zynq.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity kria_zynq is
+end kria_zynq;
+
+architecture rtl of kria_zynq is
+begin
+
+  u_zynq_wrapper : entity work.zynq_wrapper;
+  -- empty port map for now
+
+end rtl;
