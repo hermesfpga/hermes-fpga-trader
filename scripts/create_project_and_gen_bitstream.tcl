bf4a71745d46cba8a158fc8eb9b87e76e48cdd21
hermes <hermes.fpga.dev@gmail.com>
2026-03-18 23:40:09 +0100

    Limiting number of cores used for build on linux, copy netlist
    limit the number of cores to decrease peak memory usage.
    otherwise memory usage is not limited, which can cause wsl crash
    and failing builds.
    also copying the netlist for ILA probing to the dt folder

--- scripts/create_project_and_gen_bitstream.tcl
+++ scripts/create_project_and_gen_bitstream.tcl
@@ -3,15 +3,32 @@ puts $TCLPATH
 source $TCLPATH/build_utils.tcl
 source $TCLPATH/setup_project.tcl
 
+# Thread count constants (easily modifiable)
+set SYNTH_JOBS_WINDOWS 4
+set SYNTH_JOBS_LINUX 20
+set IMPL_JOBS_WINDOWS 4
+set IMPL_JOBS_LINUX 10
+
+# Detect if running on Linux, assign appropriate job counts
+if {[string equal $tcl_platform(os) "Linux"]} {
+    set SYNTH_JOBS $SYNTH_JOBS_LINUX
+    set IMPL_JOBS $IMPL_JOBS_LINUX
+    puts "Running on Linux - Synth jobs: $SYNTH_JOBS, Impl jobs: $IMPL_JOBS"
+} else {
+    set SYNTH_JOBS $SYNTH_JOBS_WINDOWS
+    set IMPL_JOBS $IMPL_JOBS_WINDOWS
+    puts "Running on Windows - Synth jobs: $SYNTH_JOBS, Impl jobs: $IMPL_JOBS"
+}
+
 # Launch synthesis on top level
 reset_run synth_1
-launch_runs synth_1 -jobs 20
+launch_runs synth_1 -jobs $SYNTH_JOBS
 wait_on_run synth_1
 
 # Launch implementation on top level
 reset_run impl_1
 set_property strategy Performance_Explore [get_runs impl_1]
-launch_runs impl_1 -to_step write_bitstream -jobs 10
+launch_runs impl_1 -to_step write_bitstream -jobs $IMPL_JOBS
 wait_on_run impl_1
 
 # One-line report + checks
